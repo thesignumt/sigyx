8d45b0dd07314e55fca7dd73d9128353a58870cb
thesignumt <tcrrxzz@gmail.com>
2025-11-04 08:16:08 +0000

    chore(parser): cmd is now str

--- sigyx/parser.py
+++ sigyx/parser.py
@@ -4,12 +4,10 @@ from dataclasses import dataclass
 
 @dataclass
 class ParsedCmd:
-    cmd: str | None
+    cmd: str
     args: list[str]
 
 
 def parse(cmd_str: str) -> ParsedCmd:
     parts = shlex.split(cmd_str)
-    if not parts:
-        return ParsedCmd(None, [])
     return ParsedCmd(parts[0], parts[1:])
